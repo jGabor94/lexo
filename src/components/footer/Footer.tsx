aa63ce21fe92c9505791fac4471b918ec1df2523
Jakucs <jakucs.gabor94@gmail.com>
2024-09-28 08:15:34 +0200

    add version

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,5 @@
 import GitHubIcon from '@mui/icons-material/GitHub';
-import { Divider, Link, Stack } from "@mui/material";
+import { Divider, Link, Stack, Typography } from "@mui/material";
 import { FC, Fragment } from "react";
 import ChangeLog from "./components/changeLog/ChangeLog";
 import PrivacyPolicy from "./components/PrivacyPolicy";
@@ -7,18 +7,26 @@ import PrivacyPolicy from "./components/PrivacyPolicy";
 const Footer: FC<{}> = () => {
     return (
         <Fragment>
-            <Divider sx={{ width: "100%" }} />
-            <Stack direction="row" gap={2} justifyContent="center" p={4} flexWrap="wrap">
-                <PrivacyPolicy />
-                <Divider flexItem orientation="vertical" />
-                <ChangeLog />
-                <Divider flexItem orientation="vertical" />
-                <Stack direction="row" gap={0.5} alignItems="center">
-                    <GitHubIcon sx={{ width: 20 }} />
-                    <Link href="https://github.com/jGabor94/lexo" sx={{ textDecoration: "none" }}>Source code</Link>
+            <Divider sx={{ width: "100%", mb: 3 }} />
+            <Stack alignItems="center">
+                <Stack gap={1} alignItems="center">
+                    <Stack direction="row" gap={2} justifyContent="center" flexWrap="wrap">
+                        <PrivacyPolicy />
+                        -
+                        <ChangeLog />
+                        -
+                        <Stack direction="row" gap={0.5} alignItems="center">
+                            <GitHubIcon sx={{ width: 20 }} />
+                            <Link href="https://github.com/jGabor94/lexo" sx={{ textDecoration: "none" }}>Source code</Link>
+                        </Stack>
+                    </Stack>
+                    <Divider flexItem />
+                    <Typography fontSize={13}> v0.1.0 (Alpha)</Typography>
                 </Stack>
+
             </Stack>
-        </Fragment>
+
+        </Fragment >
 
     )
 }
