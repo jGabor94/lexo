799b41d47646037b40109b4c8a3708e61ac0d90c
Jakucs <jakucs.gabor94@gmail.com>
2024-09-29 14:49:22 +0200

    add loading to the removeFromFolder

--- src/components/folder/RemoveFromFolder.tsx
+++ src/components/folder/RemoveFromFolder.tsx
@@ -6,26 +6,35 @@ import { SetListItem } from '@/lib/database/queries/getSets';
 import RemoveIcon from '@mui/icons-material/Remove';
 import { ListItemIcon, ListItemText, MenuItem, } from '@mui/material';
 import { useParams } from 'next/navigation';
-import { FC } from 'react';
+import { FC, Fragment, useState } from 'react';
+import LinearLoading from '../LinearLoading';
 
 const RemoveFromFolder: FC<{ set: SetListItem }> = ({ set }) => {
+
+    const [loading, setLoading] = useState(false)
 
     const { folderid } = useParams<{ folderid: string }>()
 
     const { action: removeFromFolder } = useAction(SA_RemoveFromFolder)
 
     const handleCLick = async () => {
+        setLoading(true)
         await removeFromFolder(folderid, set._id)
+        setLoading(false)
     }
 
 
     return (
-        <MenuItem onClick={handleCLick}>
-            <ListItemIcon>
-                <RemoveIcon fontSize="small" />
-            </ListItemIcon>
-            <ListItemText>Remove</ListItemText>
-        </MenuItem>
+        <Fragment>
+            <LinearLoading {...{ loading }} />
+            <MenuItem onClick={handleCLick}>
+                <ListItemIcon>
+                    <RemoveIcon fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>Remove</ListItemText>
+            </MenuItem>
+        </Fragment>
+
     )
 }
 
