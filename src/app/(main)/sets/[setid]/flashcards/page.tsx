44a8e1740b379fcd147384cc59caa895c9ba70c7
Jakucs <jakucs.gabor94@gmail.com>
2024-10-17 18:39:16 +0200

    change flashcard mode selector overlay

--- src/app/(main)/sets/[setid]/flashcards/page.tsx
+++ src/app/(main)/sets/[setid]/flashcards/page.tsx
@@ -1,9 +1,10 @@
 "use client"
 
+import TextLine from '@/components/ui/TextLine'
 import useSet from '@/lib/hooks/useSet'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack'
 import StyleOutlinedIcon from '@mui/icons-material/StyleOutlined'
-import { Box, Divider, Paper, Stack, Typography } from '@mui/material'
+import { Paper, Stack, Typography } from '@mui/material'
 import Link from 'next/link'
 import { FC } from 'react'
 
@@ -14,20 +15,20 @@ const Page: FC<{}> = () => {
     return (
 
         <Stack gap={3}>
-            <Stack direction="row" gap={2} alignItems="center">
-                <Stack direction="row" alignItems="center" gap={2}>
-                    <Typography sx={{ textWrap: "nowrap", fontSize: 35 }}>
-                        {set.name}
-                    </Typography>
-                    <Divider orientation="vertical" flexItem />
-                    <StyleOutlinedIcon sx={{ width: 40, height: 40 }} />
-                </Stack>
-                <Box sx={{ width: "100%", height: "1px", backgroundColor: "divider" }} />
+            <Stack direction="row" justifyContent="space-between" alignItems="center" gap={2} >
+                <TextLine>
+                    <Stack direction="row" gap={1} sx={{ flexShrink: 1, minWidth: 0, alignItems: "center" }}>
+                        <StyleOutlinedIcon sx={{ width: 40, height: 40 }} />
+                        <Typography sx={{ overflow: "hidden", whiteSpace: "nowrap", fontSize: 25, textOverflow: "ellipsis", maxWidth: "100%" }}>{set.name}</Typography>
+                    </Stack>
+                </TextLine>
+
                 <Stack direction="row" gap={0.5} component={Link} href={`/sets/${set._id}`} sx={{ textWrap: "nowrap", color: "text.primary", textDecoration: "none" }}>
                     <ArrowBackIcon />
                     <Typography>Back to the set</Typography>
                 </Stack>
             </Stack>
+
             <Stack gap={2} sx={{ flexDirection: { xs: "column", sm: "row" } }}>
 
                 <Link href={`/sets/${set._id}/flashcards/free`} legacyBehavior>
@@ -38,7 +39,7 @@ const Page: FC<{}> = () => {
                         flex: 1,
                         cursor: "pointer"
                     }}>
-                        Free
+                        Free mode
                     </Paper>
                 </Link>
                 <Link href={`/sets/${set._id}/flashcards/progress`} legacyBehavior>
@@ -49,7 +50,7 @@ const Page: FC<{}> = () => {
                         flex: 1,
                         cursor: "pointer"
                     }}>
-                        Progress
+                        Progress mode
                     </Paper>
                 </Link>
 
