9093004d7cdc7788659c9cf37e33f419f5466169
Jakucs <jakucs.gabor94@gmail.com>
2024-10-19 11:17:08 +0200

    change login page to static

--- src/app/(guest)/login/page.tsx
+++ src/app/(guest)/login/page.tsx
@@ -5,26 +5,26 @@ import { env } from "process";
 import { FC, Fragment } from "react";
 import CredentialsSingIn from "../../../components/CredentialsSingIn";
 
+export const dynamic = "force-static"
+
 const Page: FC<{}> = () => {
     return (
-        <Fragment>
-            <Stack sx={{ width: "100%", height: "80vh", position: "relative" }} alignItems="center" justifyContent="center" gap={3} p={2}>
-                <AnimatedLogo />
-                <Typography sx={{ textAlign: "center" }}>
-                    Create your own collections and practice them through different tasks.
-                </Typography>
-                <Stack sx={{ width: 300, maxWidth: "100%", gap: 3 }}>
-                    <GoogleSignInButton />
-                    {env.NODE_ENV === "development" && (
-                        <Fragment>
-                            <Divider flexItem>or</Divider>
-                            <CredentialsSingIn />
-                        </Fragment>
-                    )}
-                </Stack>
-
+        <Stack sx={{ width: "100%", height: "80vh", position: "relative" }} alignItems="center" justifyContent="center" gap={3} p={2}>
+            <AnimatedLogo />
+            <Typography sx={{ textAlign: "center" }}>
+                Create your own collections and practice them through different tasks.
+            </Typography>
+            <Stack sx={{ width: 300, maxWidth: "100%", gap: 3 }}>
+                <GoogleSignInButton />
+                {env.NODE_ENV === "development" && (
+                    <Fragment>
+                        <Divider flexItem>or</Divider>
+                        <CredentialsSingIn />
+                    </Fragment>
+                )}
             </Stack>
-        </Fragment>
+
+        </Stack>
     )
 }
 
