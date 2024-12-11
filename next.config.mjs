d826c063616519cdb61e9f5dd008b055bdc50841
Jakucs <jakucs.gabor94@gmail.com>
2024-12-11 19:22:02 +0100

    change  env statement

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        NEXTAUTH_URL: !process.env.VERCEL_ENV ? 'http://localhost:3000' : `https://${process.env.VERCEL_URL}`,
-        AUTH_URL: !process.env.VERCEL_ENV ? 'http://localhost:3000/api/auth' : `https://${process.env.VERCEL_URL}/api/auth`
+        NEXTAUTH_URL: !process.env.VERCEL_ENV ? 'http://localhost:3000' : process.env.VERCEL_ENV === "poduction" ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}` : `https://${process.env.VERCEL_URL}`,
+        AUTH_URL: !process.env.VERCEL_ENV ? 'http://localhost:3000/api/auth' : process.env.VERCEL_ENV === "poduction" ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}/api/auth` : `https://${process.env.VERCEL_URL}/api/auth`
     },
     experimental: {
         staleTimes: {
